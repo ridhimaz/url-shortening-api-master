1f54ec5756e281726cbe95727a57bd0950d2375c
Ridhima Kohli <ridhimakohli@Ridhimas-MacBook-Air.local>
2023-12-14 10:05:19 +0530

    api updated

--- src/components/Shortener.js
+++ src/components/Shortener.js
@@ -1,83 +1,107 @@
 import { useEffect, useState } from 'react'
-// import bgMobile from '../images/bg-shorten-mobile.svg'
-// import bgDesktop from '../images/bg-shorten-desktop.svg'
+import "../components/Shortenerstyles.css"
 
-const getLocalStorage=()=>{
-  let links =localStorage.getItem("links")
-  if(links)
+const getLocalStorage = () => {
+  let links = localStorage.getItem("links")
+  if (links) {
+    return localStorage.getItem("links")
+  }
+  else {
+    return ;
+  }
+}
+const getOriginalLink =() => {
+  let original_link=localStorage.getItem("original_link");
+  if(original_link)
   {
-    return JSON.parse(localStorage.getItem("links"))
+    return original_link;
   }
   else{
-    return []
+    return ;
   }
 }
 export default function Shortener() {
   const [text, setText] = useState("")
+  const [original_link,setOriginal_link]=useState(getOriginalLink())
   const [links, setLinks] = useState(getLocalStorage())
   const [copy, setcopy] = useState("Copy");
+
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!text) {
       alert("Input is empty")
     }
     else {
-    
-     
       const shortenLink = async () => {
-        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${text}`)
-       
-        
-        if(!res.ok){
-          document.getElementById('shorten-input').style.borderStyle = "solid";
-          document.getElementById('shorten-input').style.borderColor = 'hsl(0, 87%, 67%)';
-          document.querySelector('.error-msg').innerHTML = 'Please enter a valid link';
-         
-    }
+        const url = 'https://url-shortener-service.p.rapidapi.com/shorten';
+        const options = {
+          method: 'POST',
+          headers: {
+            'content-type': 'application/x-www-form-urlencoded',
+            'X-RapidAPI-Key': '77250017a6mshcab731390f85fd8p10d64cjsn7c7faa4f44e4',
+            'X-RapidAPI-Host': 'url-shortener-service.p.rapidapi.com'
+          },
+          body: new URLSearchParams({
+            url: `${text}`
+          })
+        };
 
-        const data = await res.json()
-        console.log(data)
-        setLinks(data.result)
-        setText("")
+        try {
+          const res = await fetch(url, options);
+          if (!res.ok) {
+            document.getElementById('shorten-input').style.borderStyle = "solid";
+            document.getElementById('shorten-input').style.borderColor = 'hsl(0, 87%, 67%)';
+            document.querySelector('.error-msg').innerHTML = 'Please enter a valid link';
+          }
+
+          const result = await res.json();
+          console.log(result.result_url);
+          setLinks(result.result_url);
+          setOriginal_link(text);
+          console.log(original_link);
+          setText("")
+        } catch (error) {
+          console.error(error);
+        }
       }
-      shortenLink()
+      shortenLink();
     }
   }
   const handleCopy = () => {
-    navigator.clipboard.writeText(links.full_short_link);
+    navigator.clipboard.writeText(links);
     setcopy("Copied!")
   }
-  useEffect(()=>{
-    localStorage.setItem("links",JSON.stringify(links))
+  useEffect(() => {
+    localStorage.setItem("links", links)
   }, [links])
+
+  useEffect(() => {
+    localStorage.setItem("original_link", original_link)
+  }, [original_link])
+
   return (
     <>
       <section>
         <div className='shorten text-center'>
-         
-            <form onSubmit={handleSubmit} className='shortenForm'>
-              <input id="shorten-input" type="url" placeholder=' Shorten a link here...' value={text}
-                onChange={(e) => setText(e.target.value)}
-              />
-              <button id='shorten-btn' type="submit" onClick={handleSubmit}>Shorten It!</button>
-              <div className='error-msg'></div>
-            </form>
-        
-          <div className='container'>
-          <div className='row  '>
-          <article className='col-lg-7 original-link'>
-              <h6>{links.original_link.slice(0,50)}...</h6>
-            </article>
-            <article className='col-lg-5 short_link'>
-                <h6>{links.full_short_link}</h6>
-                <button onClick={handleCopy} id='copy'>{copy}</button>
-             
-            </article>
-          </div>
-            
 
+          <form onSubmit={handleSubmit} className='shortenForm'>
+            <input id="shorten-input" type="url" placeholder=' Shorten a link here...' value={text}
+              onChange={(e) => setText(e.target.value)} />
+            <button id='shorten-btn' type="submit" onClick={handleSubmit}>Shorten It!</button>
+          
+          </form>
+
+          <div className='container'>
+         
+            
+             <h6>{original_link.length > 50 ? original_link.slice(0, 50) : original_link}...</h6>
            
-          </div>
+              <article className='links'>
+                <h6>{links}</h6>
+                <button onClick={handleCopy} id='copy'>{copy}</button>
+              </article>
+            </div>
+         
         </div>
       </section>
     </>
